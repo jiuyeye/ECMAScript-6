60a2eb1660dbc2193d1c87bdcef6b40fda9c94d6
lcq0202 <lcq0202@163.com>
2017-12-11 18:37:57 +0800

    数据解构赋值

---
+++ day03/main.js
@@ -0,0 +1,15 @@
+'user strict';
+var arr=[1,2,3];
+// console.log(arr[0]);
+
+
+var [a,b,c] = [1,2,3,4];
+var [d=1,e=2] = [];
+var [x=1,y=2] = [3,5];
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(x);
+console.log(y);
