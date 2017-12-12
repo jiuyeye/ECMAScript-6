53edd46989a439a2160dffd03e876330894dc951
lcq0202 <lcq0202@163.com>
2017-12-12 14:46:31 +0800

    class constructor

--- day06/main.js
+++ day06/main.js
@@ -1,9 +1,38 @@
 'use strict';
 
+// class Person{
+//     say(){
+//         console.log('hello');
+//     }
+// }
+// var p = new Person();
+// p.say();
+
+
+// class继承
 class Person{
+    constructor(name){
+        this.name = name;
+    }
     say(){
-        console.log('hello');
+        console.log(this.name+'说话了');
     }
 }
-var p = new Person();
+var p = new Person('Jack');
 p.say();
+
+class Man extends Person{
+    constructor(name){
+        super(name);
+    }
+}
+var m = new Man('Black');
+m.say();
+
+class Women extends Person{
+    constructor(name){
+        super(name);
+    }
+}
+var w = new Women('Black');
+w.say();
