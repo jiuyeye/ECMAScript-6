b09774a908b026ba67ddbca7c2aa8250a6a370e3
lcq0202 <lcq0202@163.com>
2017-12-12 19:29:54 +0800

    es6 export

--- day08/default.js
+++ day08/default.js
@@ -1,5 +1,12 @@
 'use strict';
 
-export default function say(){
-    console.log(111);
+// export default function say(){
+//     console.log(111);
+// }
+// export default function say(a,b){
+//     console.log(a,b);
+// }
+
+export default function say(...data){
+    console.log(...data);
 }
--- day08/main.js
+++ day08/main.js
@@ -1,3 +1,4 @@
 'use strict';
+
 import fn from './default';
-fn();
+fn(1,3,5,6);
